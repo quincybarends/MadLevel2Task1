679f07c0e490ab93fdf107cb4791f95d87bc7b3b
quincyba <qbarend@gmail.com>
2020-10-23 06:07:07 +0200

    setting up MainActivity.kt

--- app/src/main/java/com/example/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.madlevel2task1.databinding.ActivityMainBinding
+import layout.PlaceAdapter
 
 class MainActivity : AppCompatActivity() {
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews(){
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i], Place.PLACE_RES_DRAWABLE_IDS[i]))
+
+        }
+        placeAdapter.notifyDataSetChanged()
     }
 }
